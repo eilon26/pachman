dcc4ca068c36869c585ac16097f0876723f2beec
EILON <EILON@EILON-LAPTOP>
2018-11-26 16:50:33 +0200

    azimut altitude

--- OOP_EX2-Ex4/src/Coords/CoordsTest.java
+++ OOP_EX2-Ex4/src/Coords/CoordsTest.java
@@ -10,25 +10,25 @@ class CoordsTest {
 
 	@Test
 	void AddTest() {
-		Point3D p = new Point3D(35.209039,32.103315,670);
+		Point3D p = new Point3D(32.103315,35.209039,670);
 		MyCoords x = new MyCoords();
-		Point3D vector = new Point3D(-359.249206,337.698992,-20);
-		Point3D expected = new Point3D(35.20522500000997,32.10635199999945,650.0);
+		Point3D vector = new Point3D(337.698992,-359.249206,-20);
+		Point3D expected = new Point3D(32.10635199999945,35.20522500000997,650.0);
 		assertEquals(expected.toString(),x.add(p, vector).toString());
 	}
 	@Test
 	void DistanceTest() {
-		Point3D p = new Point3D(35.209039,32.103315,670);
-		Point3D p2 = new Point3D(35.205225,32.106352,650);
+		Point3D p = new Point3D(32.103315,35.209039,670);
+		Point3D p2 = new Point3D(32.106352,35.205225,650);
 		MyCoords x = new MyCoords();
 		assertEquals(493.05233183241336, x.distance3d(p, p2));
 	}
 	@Test
 	void VectorTest() {
-		Point3D p = new Point3D(35.209039,32.103315,670);
+		Point3D p = new Point3D(32.103315,35.209039,670);
 		MyCoords x = new MyCoords();
-		Point3D p2 = new Point3D(35.205225,32.106352,650);
-		Point3D expected = new Point3D(-359.249206,337.698992,-20);
+		Point3D p2 = new Point3D(32.106352,35.205225,650);
+		Point3D expected = new Point3D(337.698992,-359.249206,-20);
 		Point3D actual = x.vector3D(p, p2);
 		double actualX= (int)(actual.x()*1000000);
 		double actualY= (int)(actual.y()*1000000);
@@ -38,18 +38,32 @@ class CoordsTest {
 		actualZ/=1000000;
 		assertEquals(expected.toString(), new Point3D(actualX,actualY,actualZ).toString());
 	}
-	@Test
-	void AzimuthTest() {
-		Point3D p = new Point3D(35.209039,32.103315,670);
-		MyCoords x = new MyCoords();
-	}
+
 	@Test
 	void isValidTest() {
-		Point3D p = new Point3D(35.209039,32.103315,670);
+		Point3D p = new Point3D(32.103315,35.209039,670);
 		MyCoords x = new MyCoords();
 		assertTrue(x.isValid_GPS_Point(p));
 	}
+	@Test
+	void azimuth_elevation_dist_Test() {
+		MyCoords x = new MyCoords();
+		double[] polarVec;
+		Point3D p1 = new Point3D(33.112109,35.789906,989);
+		Point3D p2 = new Point3D(33.115083,35.719633,788);
+		
+		polarVec = x.azimuth_elevation_dist(p1,p2);
+		assertEquals(272,(int)(polarVec[0]));
+		assertEquals(-1.7,(double)((int)(polarVec[1]*10))/10);
+		assertEquals(6553,(int)(polarVec[2]/100));
+//		for (int i=0;i<3;i++) {
+//			System.out.println(polarVec[i]+"  ");
+//		}
+		
 
+	}
+	
+	
 
 
 
--- OOP_EX2-Ex4/src/Coords/MyCoords.java
+++ OOP_EX2-Ex4/src/Coords/MyCoords.java
@@ -7,17 +7,17 @@ public class MyCoords implements coords_converter {
 	public static final int earthR = 6371000;
 	@Override
 	public Point3D add(Point3D gps, Point3D local_vector_in_meter) {
-		double LonNorm = Math.cos(Math.toRadians(gps.y()));
-		double y1 = Math.toDegrees(Math.asin(local_vector_in_meter.y()/earthR))+gps.y();
-		double x1 = Math.toDegrees(Math.asin(local_vector_in_meter.x()/(earthR*LonNorm)))+gps.x();
+		double LonNorm = Math.cos(Math.toRadians(gps.x()));
+		double y1 = Math.toDegrees(Math.asin(local_vector_in_meter.y()/(earthR*LonNorm)))+gps.y();
+		double x1 = Math.toDegrees(Math.asin(local_vector_in_meter.x()/(earthR)))+gps.x();
 		double z1 = local_vector_in_meter.z()+gps.z();
 		return new Point3D(x1,y1,z1);
 }
 	@Override
 	public double distance3d(Point3D gps0, Point3D gps1) {
-		double LonNorm = Math.cos(Math.toRadians(gps0.y()));
-		double meterX = LonNorm*earthR*Math.sin(Math.toRadians(gps1.x()-gps0.x()));
-		double meterY = earthR*Math.sin(Math.toRadians(gps1.y()-gps0.y()));
+		double LonNorm = Math.cos(Math.toRadians(gps0.x()));
+		double meterX = earthR*Math.sin(Math.toRadians(gps1.x()-gps0.x()));
+		double meterY = LonNorm*earthR*Math.sin(Math.toRadians(gps1.y()-gps0.y()));
 		return Math.sqrt((meterX*meterX)+(meterY*meterY));
 	}
 
@@ -25,8 +25,8 @@ public class MyCoords implements coords_converter {
 	public Point3D vector3D(Point3D gps0, Point3D gps1) {
 		double meterY = earthR*Math.sin(Math.toRadians(gps1.y()-gps0.y()));
 		double meterX = earthR*Math.sin(Math.toRadians(gps1.x()-gps0.x()));
-		double LonNorm = Math.cos(Math.toRadians(gps0.y()));
-		meterX *= LonNorm;
+		double LonNorm = Math.cos(Math.toRadians(gps0.x()));
+		meterY *= LonNorm;
 		double meterZ = gps1.z()-gps0.z();
 		return new Point3D(meterX,meterY,meterZ);
 	}
@@ -34,23 +34,34 @@ public class MyCoords implements coords_converter {
 	@Override
 	public double[] azimuth_elevation_dist(Point3D gps0, Point3D gps1) {
 		double[] polarVec = new double[3];
-		double dY = gps1.y()-gps0.y();
-		double dX = gps1.x()-gps0.x();
-		double dZ = gps1.z()-gps0.z();
-		polarVec[0] = Math.atan(Math.abs((dY)/(dX)));
-		if ((dX<0)&&(dY<0)) polarVec[0]+=180; 
-		if ((dX<0)&&(dY>0)) polarVec[0]=180-polarVec[0]; 
-		if ((dX>0)&&(dY<0)) polarVec[0]=360-polarVec[0];
-		polarVec[1] = Math.acos(dZ);
-		if (polarVec[1]>180) polarVec[1]=360-polarVec[1];
-		polarVec[2] = distance3d(gps0,gps1);
-		return polarVec;
+		if (isValid_GPS_Point(gps0)&&isValid_GPS_Point(gps1)&&(distance3d(gps0,gps1)<=100000)) {
+			Point3D vec = vector3D(gps0,gps1);
+			polarVec[2] = distance3d(gps0,gps1);
+			polarVec[0] = Math.toDegrees(Math.atan(Math.abs((vec.y())/(vec.x()))));
+			if ((vec.x()<0)&&(vec.y()>0)) polarVec[0]=180-polarVec[0];
+			if ((vec.y()<0)&&(vec.x()>0)) polarVec[0]=360-polarVec[0];
+			if ((vec.x()<0)&&(vec.y()<0)) polarVec[0]=180+polarVec[0];
+			if (polarVec[0]<0) polarVec[0]+=360;
+			if (polarVec[0]>360) polarVec[0]-=360;
+			
+			polarVec[1] = Math.toDegrees(Math.asin(vec.z()/polarVec[2]));
+			
+			return polarVec;
+		} else {
+			try {
+				throw new Exception("illigal value");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
 	}
 
 	@Override
 	public boolean isValid_GPS_Point(Point3D p) {
-		if (p.x()>=-180 && p.x()<=180) {
-			if (p.y()>=-90 && p.y()<=90) {
+		if (p.y()>=-180 && p.y()<=180) {
+			if (p.x()>=-90 && p.x()<=90) {
 				if (p.z()>=-450) {
 					return true;
 				}
