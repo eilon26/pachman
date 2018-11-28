716619aa80da21e99e4a96c3a7040360a03e21ff
EILON <EILON@EILON-LAPTOP>
2018-11-28 21:32:48 +0200

    s

--- OOP_EX2-Ex4/src/algorithms/MultiCSV.java
+++ OOP_EX2-Ex4/src/algorithms/MultiCSV.java
@@ -18,7 +18,6 @@ public class MultiCSV {
 	public void project2kml(String output) {
 		String kmlstart = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n" + 
 				"<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Style id=\"red\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/red-dot.png</href></Icon></IconStyle></Style><Style id=\"yellow\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/yellow-dot.png</href></Icon></IconStyle></Style><Style id=\"green\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/green-dot.png</href></Icon></IconStyle></Style><Folder><name>Wifi Networks</name>\r\n";
-		
 		StringBuilder content = new StringBuilder(kmlstart);
 		String kmlend = "</Folder>\r\n" + "</Document></kml>";
 		try{
